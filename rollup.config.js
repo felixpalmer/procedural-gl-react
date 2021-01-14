423215b3f061268867fd37534ad7fcea7bdab8e3
Felix Palmer <felixpalmer@gmail.com>
2021-01-14 17:56:42 +0100

    Don’t dupe paths

--- rollup.config.js
+++ rollup.config.js
@@ -3,16 +3,18 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 
+import pkg from './package.json';
+
 export default {
   input: 'src/ProceduralMap',
   output: [
     {
-      file: 'build/procedural-gl-react.module.js',
+      file: pkg.module,
       format: 'esm'
     },
     {
       name: 'ProceduralMap',
-      file: 'build/procedural-gl-react.js',
+      file: pkg.main,
       format: 'umd'
     }
   ],
